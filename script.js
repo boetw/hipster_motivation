0b4852ccc34102a3071b05b3c004ab5862203311
Boet Waldbjorn <boetw@me.com>
2017-03-31 15:06:38 -0700

    styling

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  var height = $(window).height();
-  console.log('window height ' + height);
+  var height = $(document).height();
+  console.log('window height ' + $(window).height());
     console.log('document height ' + $(document).height());
 
-  var width = $(window).width();
+  var width = $(document).width();
   var half = width / 2;
-  console.log('window width ' + width);
+  console.log('window width ' + $(window).width());
     console.log('document width ' + $(document).width());
 
   $(document.body).css("width", width);
   $(document.body).css("height", height);
   $(document.body).css("background", "url(http://unsplash.it/" + width + "/" + height + ") no-repeat");
-  if (width < 800) {
+  if (width < 10) {
     $("#arrow-up").css("border-bottom", width + "px solid black");
     $("#arrow-up").css("border-left", half + "px solid transparent");
     $("#arrow-up").css("border-right", half + "px solid transparent");
